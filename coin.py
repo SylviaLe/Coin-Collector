f43bfba5657c242110fe4ff44da1b3d4c017632b
SylviaLe <sylviale19112001@gmail.com>
2019-12-02 18:52:17 -0500

    Change coin scatter

--- coin.py
+++ coin.py
@@ -5,20 +5,28 @@ class Coin():
     def __init__(self, num, window):
         self.num = num
         self.coinList = []
-        self.coinFile = 'coin.png'
+        self.drawnCoin = []
+        self.coinFile = 'coin(1).png'
         self.createCoin(window)
 
     def createCoin(self, window):
-        for i in range(self.num):
-            x = randrange(10, 990)
-            y = randrange(10, 790)
-            self.coinList.append(Point(x, y))
+##        for i in range(self.num):
+##            x = randrange(10, 990)
+##            y = randrange(10, 790)
+##            self.coinList.append(Point(x, y))
+##
+##            Image(Point(x, y), self.coinFile).draw(window)
+##    
+        for x in range (10,791, 15):
+            for y in range(7, 593, 15):
+                self.coinList.append(Point(x, y))
 
-            Image(Point(x, y), self.coinFile).draw(window)
-    
-                
+        for i in range(self.num):
+            self.coinList[randrange(2120)].draw(window)
+          
 def main():
-    win = GraphWin('Coin Test', 1000, 800)
+    win = GraphWin('Coin Test', 800, 600)
+    win.setCoords(0, 0, 800, 600)
     coin = Coin(50, win)
 
     win.getMouse()
--- player.py
+++ player.py
@@ -3,37 +3,41 @@ from coin import *
 
 class Player:
     def __init__(self, window, num):
-        self.playFile = 'fox.png'
-        self.iniPos = Point(500, 400)
-        self.player = Image(self.iniPos, self.playFile)
-        self.player.draw(window)
-
         self.coinNum = num
         self.coin = Coin(self.coinNum, window)
+
+        self.playFile = 'fox.png'
+        self.iniPos = Point(400, 292)
+        self.player = Image(self.iniPos, self.playFile)
+        self.player.draw(window)
         
     def playerMove(self, window):
         key = window.getKey()
         if key == 'Up':
-            self.player.move(0, 20)
+            self.player.move(0, 15)
         elif key == 'Down':
-            self.player.move(0, -20)
+            self.player.move(0, -15)
         elif key == 'Left':
-            self.player.move(-20, 0)
+            self.player.move(-15, 0)
         elif key == 'Right':
-            self.player.move(20, 0)
+            self.player.move(15, 0)
 
     def collectCoin(self, window):
+        self.playerMove(window)
+        count = 0
         
     #def stop(): #stop the player when it hits the boundaries
         
 def main():
-    win = GraphWin('Player Test', 1000, 800)
+    win = GraphWin('Player Test', 800, 600)
+    win.setCoords(0, 0, 800, 600)
     player = Player(win, 50)
 
     while True:
-        player.playerMove(win)
+        player.collectCoin(win)
     
     win.getMouse()
     win.close()
 
-main()  
+if __name__ == '__main__':
+    main()
