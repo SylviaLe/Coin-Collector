c900e35774104889e06337aa7e32411d077d05d9
Khe Le <56932982+KheVLe@users.noreply.github.com>
2019-12-03 09:42:39 -0500

    Some changes + Problem
    + Resized window to 600x600
    + Changed variable's name (from self.playerFile to self.playerImg) (don’t get me wrong, this is just a matter of preference)
    + Changed the function of method ‘collectCoin’: anyway this part is still lengthy, needs shortening, plus we need to find another way to check membership of a 'Point' object in a list of other Points (explain below)
    ***Our problem w this code: the fox cannot collect the coins even though its anchor overlaps with the coin’s.
    ***Explanation: Python somehow cannot check for membership of a ‘Point’ object in a list of Points. I tried this in the Word Cloud assignment and here I get the same weird result. For this reason, Python doesn’t execute the rest of the codes which include ‘undraw()’ method=> we didn’t see the coin disappear.
    ***What I did: I printed the list ‘selectedPts’ and the current coordinations of the fox. For example, fox’s coords is Point(360,345). A Point object with similar coords appear in ‘selectedPts’ list but Python reads as ‘False' when checking for its membership in the list.

--- player.py
+++ player.py
@@ -6,10 +6,11 @@ class Player:
         self.coinNum = num
         self.coin = Coin(self.coinNum, window)
 
-        self.playFile = 'fox.png'
-        self.iniPos = Point(400, 292)
-        self.player = Image(self.iniPos, self.playFile)
+        self.playerImg = 'fox.png'
+        self.iniPos = Point(300,300)
+        self.player = Image(self.iniPos, self.playerImg)
         self.player.draw(window)
+        self.count = 0
         
     def playerMove(self, window):
         key = window.getKey()
@@ -23,23 +24,22 @@ class Player:
             self.player.move(15, 0)
 
     def collectCoin(self, window):
+
         self.playerMove(window)
-        count = 0
-        
-        if self.player.getAnchor() in self.coin.selectedPts:
-            print('hello')
-            count += 1
-            tempImage = Image(self.player.getAnchor(), 'coin(1).png')
-            tempImage.undraw()
-            
-    #def stop(): #stop the player when it hits the boundaries
-        
+        playerCoords = self.player.getAnchor()
+
+        if (playerCoords.getX() in self.coin.selectedX) and (playerCoords.getY() in self.coin.selectedY)
+        and (self.coin.selectedX.index(playerCoords.getX()) == self.coin.selectedY.index(playerCoords.getY())):
+            i = self.coin.selectedX.index(playerCoords.getX())
+            self.coin.selectedCoins[i].undraw()
+            self.count += 1
+       
 def main():
-    win = GraphWin('Player Test', 800, 600)
-    win.setCoords(0, 0, 800, 600)
+    win = GraphWin('Player Test', 600, 600)
+    win.setCoords(0, 0, 600, 600)
     player = Player(win, 50)
 
-    while True:
+    while player.count < 50:
         player.collectCoin(win)
     
     win.getMouse()
@@ -47,3 +47,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
